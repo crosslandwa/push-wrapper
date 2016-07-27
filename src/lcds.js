c1a62658ad482834eb73acad43cca957bb32da27
Will <crossland.w.a@gmail.com>
2016-07-27 21:50:51 +0100

    Add dependency on lodash.range. Use it to generate blank_line array and tidy up clearing LCD

--- src/lcds.js
+++ src/lcds.js
@@ -1,52 +1,31 @@
 const foreach = require('lodash.foreach'),
+    range = require('lodash.range'),
     one_to_eight = [1, 2, 3, 4, 5, 6, 7, 8],
     one_to_four = [1, 2, 3, 4],
     zero_to_seven = [0, 1, 2, 3, 4, 5, 6, 7],
     blank = 32,
+    blank_line = range(blank, 100, 0), // 68 character array filled with 'blank character'
     offsets = [0, 9, 17, 26, 34, 43, 51, 60];
 
-function LCDSegment(offset, update) {
-    var lcd_segment = this;
-    this.lcd_data = [blank, blank, blank, blank, blank, blank, blank, blank];
-
+function LCDSegment(update) {
     this.update = function(text) {
-        lcd_segment.lcd_data = lcd_data(String(text));
-        update(offset, lcd_segment.lcd_data);
+        update(lcd_data(text));
     };
 
     this.clear = function() {
-        lcd_segment.lcd_data = lcd_data(String(''));
-        update(offset, lcd_segment.lcd_data);
+        update(lcd_data(''));
     };
 }
 
 function lcd_data(text) {
+    const text_string = String(text);
     return zero_to_seven.map((index) => {
-        return text.length > index ? text.charCodeAt(index) : blank;
+        return text_string.length > index ? text_string.charCodeAt(index) : blank;
     });
 }
 
 function LCDs(send_sysex) {
     const lcds = this;
-    const update_row = function (row_number) {
-        var display_data = [];
-        foreach(one_to_eight, (channel) => {
-            display_data = display_data.concat(lcds.x[channel].y[row_number].lcd_data);
-            if ((channel % 2) == 1) display_data.push(blank);
-        });
-        send_sysex(
-            [28 - row_number]
-            .concat([0, 69, 0])
-            .concat(display_data)
-        );
-    }
-    const update_segment = function (row_number, offset, display_data) {
-        send_sysex(
-            [28 - row_number]
-            .concat([0, 9, offset])
-            .concat(display_data)
-        );
-    }
 
     this.clear = function() {
         foreach(
@@ -55,12 +34,16 @@ function LCDs(send_sysex) {
                 lcds.x[x] = { y: {} };
                 foreach(
                     one_to_four,
-                    (y) => { lcds.x[x].y[y] = new LCDSegment(offsets[x - 1], (offset, display_data) => update_segment(y, offset, display_data)) }
+                    (y) => {
+                        lcds.x[x].y[y] = new LCDSegment((display_data) => {
+                            send_sysex([28 - y].concat([0, 9, offsets[x - 1]]).concat(display_data));
+                        })
+                    }
                 )
             }
         );
 
-        foreach(one_to_four, update_row);
+        foreach(one_to_four, (row) => send_sysex([28 - row].concat([0, 69, 0]).concat(blank_line)));
     };
 
     this.x = {};
