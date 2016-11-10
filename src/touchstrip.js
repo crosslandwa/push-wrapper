2cecadb15e17345406a36396f5f42eb4d58bc9a4
crosslandwa <crossland.w.a@gmail.com>
2016-11-10 12:35:54 +0000

    Remove lodash.partial from touchstrip.js

--- src/touchstrip.js
+++ src/touchstrip.js
@@ -1,12 +1,11 @@
 const EventEmitter = require('events'),
     util = require('util'),
-    partial = require('lodash.partial'),
     handled_notes = [12];
 
 function TouchStrip() {
     EventEmitter.call(this);
-    this.receive_midi_pitch_bend = partial(receive_midi_pitch_bend, this);
-    this.receive_midi_note = partial(receive_midi_note, this);
+    this.receive_midi_pitch_bend = receive_midi_pitch_bend.bind(null, this);
+    this.receive_midi_note = receive_midi_note.bind(null, this);
     this.handled_notes = handled_notes;
 }
 util.inherits(TouchStrip, EventEmitter);
