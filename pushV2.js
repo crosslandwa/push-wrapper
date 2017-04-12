5d5e4392da8b34b9af178f651b979dee4330f843
crosslandwa <crossland.w.a@gmail.com>
2017-04-12 11:14:50 +0100

    Refactor to expose minimal API for pad

--- pushV2.js
+++ pushV2.js
@@ -4,22 +4,31 @@ const log = console.log
 const zeroToSeven = [0, 1, 2, 3, 4, 5, 6, 7]
 const oneToEight = [1, 2, 3, 4, 5, 6, 7, 8]
 
+const onPressed = (elem) => cb => { elem.on('pressed', cb); return () => elem.removeListener('pressed', cb)}
+
 module.exports = (midiOutCallBacks = []) => {
   let push = new Push({ send: bytes => { midiOutCallBacks.forEach(callback => callback(bytes)) }})
 
   oneToEight.forEach(x => {
     oneToEight.forEach(y => {
       let pad = push.grid.x[x].y[y]
-      pad.onPressed = callback => {
-        pad.on('pressed', callback)
-        return () => pad.removeListener('pressed', callback)
-      }
+      pad.onPressed = onPressed(pad)
     })
   })
 
+  let pads = oneToEight.map(x => oneToEight.map(y => {
+    let pad = push.grid.x[x].y[y]
+    return {
+      ledOn: pad.led_on.bind(pad),
+      ledOff: pad.led_off.bind(pad),
+      ledRGB: pad.led_rgb.bind(pad),
+      onPressed: pad.onPressed.bind(pad)
+    }
+  }))
+
   return {
-    gridRow: y => zeroToSeven.map(x => push.grid.x[x + 1].y[y + 1]),
-    gridCol: x => zeroToSeven.map(y => push.grid.x[x + 1].y[y + 1]),
+    gridRow: y => zeroToSeven.map(x => pads[x][y]),
+    gridCol: x => zeroToSeven.map(y => pads[x][y]),
     midiIn: bytes => push.receive_midi(bytes)
   }
 }
