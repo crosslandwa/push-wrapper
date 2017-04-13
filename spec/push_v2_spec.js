599030f00dfc69b0537aa52c72da3c227ff9ccb9
Will <crossland.w.a@gmail.com>
2017-04-13 01:10:39 +0100

    More re-use/consolidating tests

--- spec/push_v2_spec.js
+++ spec/push_v2_spec.js
@@ -3,14 +3,9 @@ fdescribe('Ableton Push wrapper', () => {
   let button, gridRow, gridCol, gridSelectButtons, midiFromHardware
 
   const midiCC = (cc, value) => [176, cc, value]
+  const midiNote = (note, velocity) => [144, note, velocity]
 
-  const executeAndCheckSentMIDI = (call, expectedMIDI) => { call(); expect(sentBytes).toEqual(expectedMIDI) }
-  const testLedOn = (elem, expected) => () => executeAndCheckSentMIDI(() => elem().ledOn(), expected)
-  const testLedOnWithValue = (elem, velocity, expected) => () => executeAndCheckSentMIDI(() => elem().ledOn(velocity), expected)
-  const testLedDim = (elem, expected) => () => executeAndCheckSentMIDI(() => elem().ledDim(), expected)
-  const testLedOff = (elem, expected) => () => executeAndCheckSentMIDI(() => elem().ledOff(), expected)
-  const testLedRGB = (elem, rgb, expected) => () => executeAndCheckSentMIDI(() => elem().ledRGB(...rgb), expected)
-
+  const testSendsMidi = ({ call: c, expect: e }) => () => { c(); expect(sentBytes).toEqual(e) }
   const testOnPressed = (elem, midi) => done => { elem().onPressed(done); midiFromHardware(midi) }
   const testOnReleased = (elem, midi) => done => { elem().onReleased(done); midiFromHardware(midi) }
 
@@ -56,30 +51,32 @@ fdescribe('Ableton Push wrapper', () => {
       midiFromHardware([160, 37, 100]); // MIDI poly key-pressure
     })
 
-    it('can have LED turned on', testLedOn(() => gridCol(7)[7], [144, 99, 100])) // default colour of 100 if 'velocity' not provided
-    it('can have LED turned on specifying velocity', testLedOnWithValue(() => gridCol(7)[6], 101, [144, 91, 101]))
-    it('can have LED turned off', testLedOff(() => gridRow(7)[6], [144, 98, 0]))
-    it('can have LED turned on with RGB values',
-      testLedRGB(() => gridRow(0)[6], [216, 80, 255], [240, 71, 127, 21, 4, 0, 8, 6, 0, 13, 8, 5, 0, 15, 15, 247])
-    )
+    it('can have LED turned on', testSendsMidi({ call: () => gridCol(7)[7].ledOn(), expect: midiNote(99, 100) }))
+    it('can have LED turned on specifying velocity', testSendsMidi({ call: () => gridCol(7)[6].ledOn(101), expect: midiNote(91, 101) }))
+    it('can have LED turned off', testSendsMidi({ call: () => gridRow(7)[6].ledOff(), expect: midiNote(98, 0) }))
+    it('can have LED turned on with RGB values', testSendsMidi(
+      { call: () => gridRow(0)[6].ledRGB(216, 80, 255), expect: [240, 71, 127, 21, 4, 0, 8, 6, 0, 13, 8, 5, 0, 15, 15, 247] }
+    ))
   })
 
   describe('grid select buttons', () => {
     it('invoke subscribed listeners when pressed', testOnPressed(() => gridSelectButtons()[0], midiCC(102, 127)))
     it('invoke subscribed listeners when released', testOnReleased(() => gridSelectButtons()[2], midiCC(104, 0)))
-    it('can have LED turned on', testLedOn(() => gridSelectButtons()[1], midiCC(103, 100))) // default colour of 100 if 'velocity' not provided
-    it('can have LED turned on', testLedOnWithValue(() => gridSelectButtons()[1], 101, midiCC(103, 101)))
-    it('can have LED turned off', testLedOff(() => gridSelectButtons()[1], midiCC(103, 0)))
-    it('can have LED turned on with RGB values',
-      testLedRGB(() => gridSelectButtons()[1], [216, 80, 255], [240, 71, 127, 21, 4, 0, 8, 65, 0, 13, 8, 5, 0, 15, 15, 247])
-    )
+
+    it('can have LED turned on', testSendsMidi({ call: () => gridSelectButtons()[1].ledOn(), expect: midiCC(103, 100) }))
+    it('can have LED turned on specifying velocity', testSendsMidi({ call: () => gridSelectButtons()[1].ledOn(101), expect: midiCC(103, 101) }))
+    it('can have LED turned off', testSendsMidi({ call: () => gridSelectButtons()[1].ledOff(), expect: midiCC(103, 0) }))
+    it('can have LED turned on with RGB values', testSendsMidi(
+      { call: () => gridSelectButtons()[1].ledRGB(216, 80, 255), expect: [240, 71, 127, 21, 4, 0, 8, 65, 0, 13, 8, 5, 0, 15, 15, 247] }
+    ))
   })
 
   describe('buttons', () => {
     it('invoked subscribed listeners when pressed', testOnPressed(() => button('AddTrack'), midiCC(53, 120)))
     it('invoked subscribed listeners when released', testOnReleased(() => button('AddEffect'), midiCC(52, 0)))
-    it('can have LED turned on', testLedOn(() => button('AddEffect'), midiCC(52, 4)))
-    it('can have LED turned on dimly', testLedDim(() => button('Play'), midiCC(85, 1)))
-    it('can have LED turned off', testLedOff(() => button('AddEffect'), midiCC(52, 0)))
+
+    it('can have LED turned on', testSendsMidi({ call: () => button('AddEffect').ledOn(), expect: midiCC(52, 4) }))
+    it('can have LED turned on dimly', testSendsMidi({ call: () => button('Play').ledDim(), expect: midiCC(85, 1) }))
+    it('can have LED turned off', testSendsMidi({ call: () => button('AddEffect').ledOff(), expect: midiCC(52, 0) }))
   })
 })
